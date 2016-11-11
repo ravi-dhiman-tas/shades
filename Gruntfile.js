26ba1c0ba0ee50c4afaa09835d4df5dc1c304d21
ravid7000 <ravi@technoarchsoftwares.com>
2016-11-11 19:46:24 +0530

    SCSS Updated

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,19 +2,25 @@ module.exports = function(grunt) {
     'use-strict';
     grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
-		sass: {
-			dist: {
-				files: {
-					'style/style.css' : 'sass/style.scss'
-				}
-			}
-		},
-		watch: {
-			css: {
-				files: '**/*.scss',
-				tasks: ['sass']
-			}
-		}
+        sass: {
+            dist: {
+                options: {
+                    style: 'compressed'
+                },
+                files: {
+                    'dist/css/shades.min.css': 'source/shades.scss'
+                }
+            }
+        },
+        watch: {
+            css: {
+                files: ['source/*.scss'],
+                tasks: ['sass'],
+                options: {
+                    spawn: false
+                }
+            }
+        },
 	});
 	grunt.loadNpmTasks('grunt-contrib-sass');
 	grunt.loadNpmTasks('grunt-contrib-watch');
