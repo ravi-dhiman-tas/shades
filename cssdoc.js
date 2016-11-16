8da39096cbbcda07738d04a369a100d7962ea853
ravid7000 <ravi@technoarchsoftwares.com>
2016-11-16 09:15:10 +0530

    Package file grunt version updated

--- cssdoc.js
+++ cssdoc.js
@@ -2,5 +2,6 @@ var Cssdoc = require('cssdoc');
 
 Cssdoc({
   inputDir: 'dist/css',    // directory with the css files
-  outputDir: './build'  // directory that will be created the page
+  outputDir: './build',  // directory that will be created the page
+  title: 'Shades'
 });
